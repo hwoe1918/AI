04f00edd499e9b05885cdb70f326d1c483e27f7e
Gyuho Cha <hwoe0205@naver.com>
2022-03-17 09:50:52 +0900

    20220317 commit2

--- 20220317/20220317 인공지능.py
+++ 20220317/20220317 인공지능.py
@@ -1,1 +1,41 @@
+#1번
 print('Hello World')
+#2번
+print("Mary's cosmetics")
+#3번
+print('신씨가 소리질렀다. "도둑이야".')
+#4번
+print('"C:\\Windows"')
+#5번
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# \n은 줄을 바꿔주는 역할, \t는 tab만큼 공백을 만들어주는 역할입니다.
+#51번
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키']
+print("{} {} {}".format(movie_rank[0],movie_rank[1],movie_rank[2]))
+#52번
+movie_rank.append("배트맨")
+print("{} {} {} {}".format(movie_rank[0],movie_rank[1],movie_rank[2],movie_rank[3]))
+#53
+movie_rank.insert(1, "슈퍼맨")
+print("{} {} {} {} {}".format(movie_rank[0],movie_rank[1],movie_rank[2],movie_rank[3], movie_rank[3]))
+#58번
+nums = [1, 2, 3, 4, 5]
+a = nums[0]+nums[1]+nums[2]+nums[3]+nums[4]
+print(a)
+#59번
+cook= ["피자","김밥","만두","양념치킨","족발","피자","김치만두","쫄면","소시지","라면","팥빙수","김치"]
+print(len(cook))
+#60번
+a /= 5.0
+print(a)
+#102번
+#False
+
+#103번
+#True
+
+#104번
+#True
+
+#105번
+#True
